1ecf78ae235464e40193363f59c00ad3bf4843c9
yuribreion <yuribreion@br.ibm.com>
2019-10-07 13:28:12 -0300

    adding more cool things

--- app/src/main/java/br/com/fiap/agendamento_fiap/MenuUser.java
+++ app/src/main/java/br/com/fiap/agendamento_fiap/MenuUser.java
@@ -1,6 +1,8 @@
 package br.com.fiap.agendamento_fiap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +13,15 @@ public class MenuUser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         getSupportActionBar().hide();
+    }
+
+    public void requestRoom(View view) {
+        Intent it = new Intent(this, RequestRoom.class);
+        startActivity(it);
+    }
+
+    public void requestMaterial(View view) {
+        Intent it = new Intent(this, RequestMaterial.class);
+        startActivity(it);
     }
 }
---
+++ app/src/main/java/br/com/fiap/agendamento_fiap/RequestMaterial.java
@@ -0,0 +1,15 @@
+package br.com.fiap.agendamento_fiap;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RequestMaterial extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_material);
+        getSupportActionBar().hide();
+    }
+}
